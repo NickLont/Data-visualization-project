df0fdf3023b44d63f23d1a7336b86e36c05f07b4
NickLont <nlontorfos@hotmail.com>
2017-02-07 00:16:21 +0200

    Auth ok, navbar username/logout ok Fixing error handlig for 500

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavBarService} from "../_services/navBarService.service";
 
 @Component({
@@ -8,11 +8,14 @@ import {NavBarService} from "../_services/navBarService.service";
 })
 
 
-export class NavbarComponent{
+export class NavbarComponent implements OnInit{
     public currentUser: string=JSON.parse(localStorage.getItem('currentUser'));
-    public state:boolean = true;
+    public state:boolean;
 
     constructor(private _navBarService : NavBarService){
         this._navBarService.navState$.subscribe(state=>this.state = state);
+    }
+    ngOnInit(){
+        this._navBarService.setNavState(false);
     }
 }
