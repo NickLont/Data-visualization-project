d5c646df8b8d8b985f118ebd2ebc772ebb4539f2
NickLont <nlontorfos@hotmail.com>
2017-05-06 15:18:09 +0300

    DAte range picker  working

--- app/stat-barcharts/stat-barcharts.component.ts
+++ app/stat-barcharts/stat-barcharts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from "../_services/data.service";
 import {Router} from "@angular/router";
-import {IMyDateRange, IMyOptions} from 'mydatepicker';
+import { IMyOptions} from 'mydaterangepicker';
 
 
 @Component({
@@ -22,17 +22,18 @@ export class StatBarChartsComponent implements  OnInit{
         {name:'2G', value:'2g'},
         {name:'3G', value:'3g'}
     ];
-    private myDatePickerOptions: IMyOptions = {
-        //  options
-        dateFormat: 'yyyy-mm-dd',
-        disableUntil: {year: 2013, month: 7, day: 20},
 
-    };
-    private myDatePickerOptions2: IMyOptions = {
-        //  options
-        dateFormat: 'yyyy-mm-dd',
+    // Date Range Options
 
+    private myDateRangePickerOptions: IMyOptions = {
+        // other options...
+        dateFormat: 'dd/mmm/yyyy',
+        inline: false,
+        disableUntil: {year:2013, month:7, day:19},
+        disableSince: {year:2014, month:7, day:14},
+        sunHighlight: false,
     };
+
 
 
     constructor( private _dataservice : DataService,
@@ -41,12 +42,15 @@ export class StatBarChartsComponent implements  OnInit{
     ngOnInit(){
         this._dataservice.getStats('levelStats','no')
             .subscribe(res => {
+                // Form Initial Values
                 this.model.type='levelStats';
                 this.model.gen='no';
-                // this.model.startDate={ date: { year: 2018, month: 10, day: 9 } };
+                this.model.dates = {beginDate: {year: 2013, month: 7, day: 20},
+                    endDate: {year: 2014, month: 7, day: 13}};
+
                 for(let r of res){
                     this.barChartLabels.push(r.operatorname)
-                    this.barChartData[0].data.push(r.avg.toF);
+                    this.barChartData[0].data.push(r.avg);
                     this.barChartData[1].data.push(r.min);
                     this.barChartData[2].data.push(r.max);
 
@@ -59,6 +63,9 @@ export class StatBarChartsComponent implements  OnInit{
             } );
 
     }
+
+    //ChartJS Options
+
     public barChartOptions:any = {
         scaleShowVerticalLines: false,
         responsive: true,
@@ -66,7 +73,7 @@ export class StatBarChartsComponent implements  OnInit{
             yAxes: [{
                 ticks: {
                     beginAtZero:true
-                    }
+                }
             }]
         }
     };
@@ -89,23 +96,45 @@ export class StatBarChartsComponent implements  OnInit{
         console.log(e);
     }
     onSubmit(){
-        console.log('to type mou einai: '+this.model.type);
-        this._dataservice.getStats(this.model.type, this.model.gen)
-        .subscribe(res => {
-            this.barChartLabels=[];
-            for(let chartData of this.barChartData){
-                chartData.data=[];
-            }
-            for(let r of res){
-                this.barChartLabels.push(r.operatorname)
-                this.barChartData[0].data.push(r.avg);
-                this.barChartData[1].data.push(r.min);
-                this.barChartData[2].data.push(r.max);
-            }
-            console.log(this.model.startDate.formatted);
-            console.log(this.model.endDate.formatted);
-            this.dataReady = true;
-        } );
+        //Check to see for Date Range entries
+        if(this.model.dates.formatted){
+            let beginDate = new Date(this.model.dates.beginEpoc*1000);
+            let endDate = new Date(this.model.dates.endEpoc*1000);
+            let formattedBeginDate : String= beginDate.getFullYear()+"-"+(beginDate.getMonth()+1)+"-"+beginDate.getDate();
+            let formattedEndDate : String= endDate.getFullYear()+"-"+(endDate.getMonth()+1)+"-"+endDate.getDate();
+
+            this._dataservice.getStats(this.model.type, this.model.gen, formattedBeginDate,formattedEndDate)
+                .subscribe(res => {
+                    this.barChartLabels=[];
+                    for(let chartData of this.barChartData){
+                        chartData.data=[];
+                    }
+                    for(let r of res){
+                        this.barChartLabels.push(r.operatorname)
+                        this.barChartData[0].data.push(r.avg);
+                        this.barChartData[1].data.push(r.min);
+                        this.barChartData[2].data.push(r.max);
+                    }
+                    this.dataReady = true;
+                } );
+        }
+        // Without Date Range
+        else{
+            this._dataservice.getStats(this.model.type, this.model.gen)
+                .subscribe(res => {
+                    this.barChartLabels=[];
+                    for(let chartData of this.barChartData){
+                        chartData.data=[];
+                    }
+                    for(let r of res){
+                        this.barChartLabels.push(r.operatorname)
+                        this.barChartData[0].data.push(r.avg);
+                        this.barChartData[1].data.push(r.min);
+                        this.barChartData[2].data.push(r.max);
+                    }
+                    this.dataReady = true;
+                } );
+        }
     }
 
 }
